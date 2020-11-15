2bdbc4895afd9a29639e04166bca2cdd39c1b63f
idanre1 <idanre1@gmx.com>
2020-11-15 13:34:22 +0200

    Update photo_title_batch.py

--- photo_title_batch.py
+++ photo_title_batch.py
@@ -1,8 +1,24 @@
+#https://stackoverflow.com/questions/42024255/bulk-join-json-with-jpg-from-google-takeout
+#https://exiftool.org/
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('--path')
+args = parser.parse_args()
+
 from pathlib import Path
 files = []
-for path in Path(args.path).rglob('*.csv.gz'):
-     20         files.append(path)
-     21 
-     22     if len(files) == 0:
-     23         print('No new files found')
-     24         exit()
+for path in Path(args.path).rglob('*.jp*g'):
+    files.append(path)
+      
+if len(files) == 0:
+    print('No new files found')
+    exit()
+
+import os
+for f in files:
+    desc = f.relative_to(args.path).parent
+    # desc=str(desc).replace('/','\n')
+    print(f'{f}:')
+    os.system(f'/usr/bin/exiftool -overwrite_original -Caption-Abstract="{desc}" -Description="{desc}" -ImageDescription="{desc}" "{f}"')
+    # quit()
