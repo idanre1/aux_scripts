b4e2c2958824d24a762cedaafbc1f159eb138479
Idan Regev <idanre1@gmx.com>
2021-06-20 16:21:34 +0300

    have i been pwnd

---
+++ hibp_query.py
@@ -0,0 +1,20 @@
+import hashlib
+import mmap
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('password')
+parser.add_argument("db" , help="", nargs='?', default='/nas/pwned-passwords-sha1-ordered-by-count-v7.txt') # Positional optional argument
+args = parser.parse_args()
+
+hash = hashlib.sha1(args.password.encode()).hexdigest().upper()
+print(f'Hash:   {hash}')
+
+with open(args.db, 'rb', 0) as file, \
+     mmap.mmap(file.fileno(), 0, access=mmap.ACCESS_READ) as s:
+    pos = s.find(str.encode(hash))
+    if pos != -1:
+        s.seek(pos)
+        print(f'Result: {s.readline().decode()}')
+    else:
+        print('Not found!')
